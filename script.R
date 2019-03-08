f8f8bfb87ea47546d616b09cc63ce1c07e147cd4
Ammar-K <eng.khaldi@gmail.com>
2019-03-08 22:53:45 +0300

    changed the colors

--- script.R
+++ script.R
@@ -2,7 +2,7 @@
 
 library(ggplot2)
 library(plotly)
-library(RColorBrewer)
+library(dplyr)
 
 # Load the data
 
@@ -10,21 +10,29 @@ risk <- read.csv("~/interactive_risk_matrix/risk.csv")
 
 # Creating heatmap background for Risk Matrix
 
+# setting the score in order to calculate the risk level
+Likelihood_score <- rep(c(1,2,4,6,12),5)
+Consequence_score <- rep(c(1,2,4,6,12),each=5)
 Likelihood <- rep(c(1:5),5)
 Consequence <- rep(c(1:5),each=5)
 df <- data.frame(Likelihood,Consequence)
-df <- mutate(df, Risk = Consequence + Likelihood)
+df <- mutate(df, risk_score = Consequence_score * Likelihood_score,
+             Risk = case_when(risk_score >= 0 & risk_score < 6 ~ 1,
+                              risk_score >= 6 & risk_score < 12 ~ 2,
+                              risk_score >= 12 & risk_score < 32  ~ 3,
+                              risk_score >= 32 ~ 4) )
 
 
-# Create the Color Pallete
-myPalette <- colorRampPalette(rev(brewer.pal(11, "RdYlGn")))
+# plotting 
 risk_p<- ggplot(df,aes(x =Likelihood, y =Consequence, fill=Risk))+
   geom_tile()+
-  scale_fill_gradientn(colours = rev(myPalette(10)),guide=FALSE)+
+  scale_fill_gradientn(colours = c("red", "orange","#EEEE00","#008000"),guide=FALSE)+
   scale_x_continuous(trans = "reverse",name= "Frequency",breaks = 0:5, expand = c(0, 0))+
   scale_y_continuous(trans = "reverse",name = "Consequence",breaks = 0:5, expand = c(0, 0))+
   coord_fixed()+
   theme_bw()+
+  geom_hline(yintercept = seq(1.5,5.5), color = "white")+
+  geom_vline(xintercept = seq(1.5,5.5), color = "white")+
   #ggtitle("SAFER per Risk Assessment")+
   theme(legend.position="bottom")+
   guides(color=guide_legend(title="Selected Plants"))+
@@ -38,7 +46,7 @@ risk_p<- ggplot(df,aes(x =Likelihood, y =Consequence, fill=Risk))+
                                "<b>Risk:</b>",Risk,"<br>",
                                "<b>Type:</b>",Type,"<br>",
                                "<b>Interim Action:</b>",Interim)))+
-  scale_color_manual(values = c("#ffa300","#009fdf","#aaaaaa")
+  scale_color_manual(values = c("#9400D3","#009fdf","#aaaaaa")
   )
 
 config(ggplotly(risk_p,tooltip = "text",width = 650,height = 700), displayModeBar=FALSE, collaborate = FALSE) %>%
